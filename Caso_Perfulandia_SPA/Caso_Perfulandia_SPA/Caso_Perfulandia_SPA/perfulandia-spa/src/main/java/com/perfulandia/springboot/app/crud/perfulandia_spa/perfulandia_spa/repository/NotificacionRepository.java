648163b3484e4c362e295430d22185857130b4f3
Simon <chile2005mena@gmail.com>
2025-06-17 20:02:34 -0400

    falta testeo

--- Caso_Perfulandia_SPA/Caso_Perfulandia_SPA/Caso_Perfulandia_SPA/perfulandia-spa/src/main/java/com/perfulandia/springboot/app/crud/perfulandia_spa/perfulandia_spa/repository/NotificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.perfulandia.springboot.app.crud.perfulandia_spa.perfulandia_spa.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.perfulandia.springboot.app.crud.perfulandia_spa.perfulandia_spa.entities.Notificacion;
-
-public interface NotificacionRepository extends CrudRepository <Notificacion, Long> {
-
-}
